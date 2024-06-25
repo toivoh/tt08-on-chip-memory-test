e3368ed9e4ab5b76743aa86ce3f95799ffd4cc30
Toivo Henningsson <toivo.h.h@gmail.com>
2024-06-25 08:03:49 +0200

    Start to fill in info.yaml, rename project, extend .gitignore

--- src/project.v
+++ src/project.v
@@ -1,27 +1,27 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Toivo Henningsson
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
+module tt_um_toivoh_on_chip_memory_test (
+		input  wire [7:0] ui_in,    // Dedicated inputs
+		output wire [7:0] uo_out,   // Dedicated outputs
+		input  wire [7:0] uio_in,   // IOs: Input path
+		output wire [7:0] uio_out,  // IOs: Output path
+		output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+		input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+		input  wire       clk,      // clock
+		input  wire       rst_n     // reset_n - low to reset
+	);
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+	// All output pins must be assigned. If not used, assign to 0.
+	assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+	assign uio_out = 0;
+	assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+	// List all unused inputs to prevent warnings
+	wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
